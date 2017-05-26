3ad5e67c62e1002f571ddc32fc734019e1ce4aad
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-05-26 15:18:13 +0200

    add upgrade-step to cleanup resources registry and update tiles_list storage

--- src/redturtle/tiles/management/setuphandlers.py
+++ src/redturtle/tiles/management/setuphandlers.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+from plone import api
+from zope.annotation.interfaces import IAnnotations
+from persistent.mapping import PersistentMapping
+from persistent.list import PersistentList
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+default_profile = 'profile-redturtle.tiles.management:default'
+uninstall_profile = 'profile-redturtle.tiles.management:uninstall'
 
 
 def post_install(context):
@@ -13,3 +24,39 @@ def uninstall(context):
     if context.readDataFile('redturtletilesmanagement_uninstall.txt') is None:
         return
     # Do something during the uninstallation of this package
+
+
+def update_tiles_list(brain):
+    item = brain.getObject()
+    annotations = IAnnotations(item)
+    old_list = annotations.get('tiles_list', [])
+    if not old_list:
+        return
+    new_list = PersistentList()
+    for tile_infos in annotations.get('tiles_list', []):
+        new_tile = PersistentMapping()
+        new_tile['tile_id'] = tile_infos['tile_id']
+        new_tile['tile_type'] = tile_infos['tile_type']
+        new_tile['tile_hidden'] = False
+        new_list.append(new_tile)
+
+    # create new attribute and clear annotations
+    item.tiles_list = PersistentMapping()
+    item.tiles_list['defaultManager'] = new_list
+    del annotations['tiles_list']
+    logger.info('updated tiles list for: {0}'.format(brain.getPath()))
+
+
+def to_1100(context):
+    """
+    """
+    logger.info('Upgrading redturtle.tiles.management to version 1.0.0')
+    context.runImportStepFromProfile(
+        'profile-redturtle.tiles.management:to_1100', 'plone.app.registry')
+    context.runImportStepFromProfile(default_profile, 'plone.app.registry')
+    logger.info('Reinstalled registry')
+    portal = api.portal.get()
+    for brain in portal.portal_catalog():
+        update_tiles_list(brain)
+
+    logger.info('migrated tiles')
